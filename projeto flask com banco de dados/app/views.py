671f0a979a37128df4a2f110519573e4ff1bf7b3
davimiguelmartins1904-crypto <davimiguelmartins1904@gmail.com>
2025-10-02 23:21:30 -0300

    Add files via upload

--- projeto flask com banco de dados/app/views.py
+++ projeto flask com banco de dados/app/views.py
@@ -11,3 +11,12 @@ def pagina1():
 @bp.route('/pagina2')
 def pagina2():
  return render_template('pagina2.html')
+
+bp.route('/pagina3')
+def pagina3():
+ return render_template('pagina3.html')
+
+bp.route('/pagina4')
+def pagina4():
+ return render_template('pagina4.html')
+
